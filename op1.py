2c6f8ab10866ab9a2ed1306f5ab7cb7eebc2c1a8
parabath <113873719+parabath@users.noreply.github.com>
2023-04-02 00:48:46 +0530

    AGE COMMENT CODE BY PYTHON

---
+++ op1.py
@@ -0,0 +1,21 @@
+x = input("Enter your age: ")
+age = float(x)
+if age < 1 :
+    print("Amazing")
+elif age < 5:
+    print("You are a baby.")
+    print("Stay with Parents")
+elif age < 10 :
+    print("You are a child")
+    print("Still Learning")
+elif age < 16 :
+    print("You are a teenager") 
+    print("Find good friends")
+elif age < 23 :
+    print("WORK HARD")
+elif age > 33 :
+    print ("How are you?")
+elif age > 100:
+    print("Wonderful")    
+else :
+    print ("have a nice day!")              
